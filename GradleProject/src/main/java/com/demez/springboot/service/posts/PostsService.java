35502fde198eabdce720443670976d3b4f677abd
develop-me-z <ohjisoo826@gamail.com>
2023-01-23 15:18:28 +0900

    update test

---
+++ GradleProject/src/main/java/com/demez/springboot/service/posts/PostsService.java
@@ -0,0 +1,37 @@
+package com.demez.springboot.service.posts;
+
+import com.demez.springboot.domain.posts.Posts;
+import com.demez.springboot.domain.posts.PostsRepository;
+import com.demez.springboot.web.dto.PostsResponseDto;
+import com.demez.springboot.web.dto.PostsSaveRequestDto;
+import com.demez.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestsDto){
+        return postsRepository.save(requestsDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
+    }
+}
